ac6f32413f75b2949beadda7f51a4e491aff5085
lakshmankokku123 <lakshmankokku123@gmail.com>
2024-04-10 12:23:52 +0530

    added long string program

---
+++ src/main/java/com/javaPractisePrograms/ReverseOfLongString.java
@@ -0,0 +1,13 @@
+package com.javaPractisePrograms;
+
+public class ReverseOfLongString {
+
+	public static void main(String[] args) {
+		
+		String s1="This is Lakshman Kokku , welcome to service now ";
+		
+		
+
+	}
+
+}
