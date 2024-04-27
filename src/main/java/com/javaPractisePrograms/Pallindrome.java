3dc73df71702b43637edc80d06fb769b4bc307da
lakshmankokku123 <lakshmankokku123@gmail.com>
2024-04-27 08:53:45 +0530

    added pallindrome program

---
+++ src/main/java/com/javaPractisePrograms/Pallindrome.java
@@ -0,0 +1,14 @@
+package com.javaPractisePrograms;
+
+public class Pallindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Pallindrome program 
+		
+		String s1="Malayalam";
+
+	}
+
+}
